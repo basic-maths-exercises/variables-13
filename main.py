4e4c3abbf628c4f6c16c1382d96fc2edb5f7e3fa
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-05 16:21:24 +0100

    Fixed answers

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+xvals = np.linspace(0,19,20)
+yvals = 0.5**xvals 
+
+plt.plot( xvals, yvals, 'ko' )
+plt.xlabel("Index")
+plt.ylabel("Geometric series")
+plt.savefig("geometric.png")
